d2d7c287cbf71fe2a57c4fe1152229b4af97a52d
souzaoe <souzaoe@hotmail.com>
2021-01-31 21:16:07 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/Client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/Client/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.Client.entities.Client;
 import com.devsuperior.Client.repositories.ClientRepository;
@@ -14,7 +15,8 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository; 
 	
-	public List<Client> findall(){
+	@Transactional(readOnly = true)
+	public List<Client> findall() {
 		return repository.findAll(); 
 	}
 
