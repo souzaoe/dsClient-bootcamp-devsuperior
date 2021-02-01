b00f8eb30a5085b8c2a1c3f26fa5d0f78d2c3f2f
souzaoe <souzaoe@hotmail.com>
2021-02-01 18:36:07 -0300

    Auditoria datas

--- backend/src/main/java/com/devsuperior/Client/entities/Client.java
+++ backend/src/main/java/com/devsuperior/Client/entities/Client.java
@@ -3,10 +3,13 @@ package com.devsuperior.Client.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,12 @@ public class Client implements Serializable {
 	private Double income; 
 	private Instant birthDate; 
 	private Integer children;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt; 
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt; 
 	
 	public Client() {		
 	}
@@ -81,15 +90,33 @@ public class Client implements Serializable {
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}	
+	
+	public Instant getCreatedAt() {
+		return createdAt;
 	}
 
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
+	}	
 
 	@Override
 	public boolean equals(Object obj) {
